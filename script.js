bb6eb42e1a95e87747267aa02a2f160a28805607
Shamsuzzoha <33594007+ArbiKhan007@users.noreply.github.com>
2025-11-14 18:06:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 //your JS code here. If required.
-const inputs = document.querySelectorAll(".code-container .code");
+const codes = document.querySelectorAll(".code");
 
-inputs.forEach((input, index) => {
-	input.addEventListener("input", () => {
-		input.value = input.value.replace(/[^0-9]/g, ""); 
+codes[0].focus();
 
-		if (input.value && index < inputs.length - 1) {
-            inputs[index + 1].focus();
+codes.forEach((code, idx) => {
+
+    code.addEventListener("input", () => {
+        code.value = code.value.replace(/[^0-9]/g, ""); // Allow digits only
+
+        if (code.value && idx < codes.length - 1) {
+            codes[idx + 1].focus();
         }
-	});
+    });
 
-	input.addEventListener("keydown", (e)=> {
-		if(e.key==="Backspace"&&!input.value&&index>0){
-			inputs[index-1].focus();
-		}
-	});
+    code.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace") {
+            if (code.value === "" && idx > 0) {
+                codes[idx - 1].focus();
+                codes[idx - 1].value = "";
+            }
+        }
+    });
+
 });
